f1bec9271d7fa051fa002d179585d9a4edc493f8
ForDzq <2187758898@qq.com>
2019-11-21 16:44:02 +0800

    change

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
     <home-header></home-header>
+    <home-swiper></home-swiper>
   </div>
 </template>
 <script>
 import HomeHeader from 'pages/home/components/HomeHeader'
+import HomeSwiper from 'pages/home/components/HomeSwiper'
 export default {
   name: 'Home',
   components: {
-    HomeHeader
+    HomeHeader,
+    HomeSwiper
   }
 }
 </script>
---
+++ src/pages/home/components/HomeSwiper.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgurl" alt="图片">
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination ',
+        loop: true
+      },
+      swiperList: [{
+        id: '001',
+        imgurl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201911/774fc492deb29b9bf112ebd1f8d277f5.jpg_750x200_d85df43a.jpg'
+      }, {
+        id: '002',
+        imgurl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201911/774fc492deb29b9bf112ebd1f8d277f5.jpg_750x200_d85df43a.jpg'
+      }, {
+        id: '003',
+        imgurl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201911/774fc492deb29b9bf112ebd1f8d277f5.jpg_750x200_d85df43a.jpg'
+      }
+      ]
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 26.7%
+    background: #eee
+    .swiper-img
+      width: 100%
+
+</style>
